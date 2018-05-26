905a8afae9ea6395cb03cedb7bf0a984fa3356d2
Ariel Zhen <zhenpingping@live.com>
2018-05-26 05:24:56 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,17 @@ def countdown(num)
   counter = num
   until counter == 0 do
     puts "#{counter} SECOND(S)!"
-    counter -= 1 
+    counter -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  counter = num
+  until counter == 0 do
+    sleep(1)
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
   end
   return "HAPPY NEW YEAR!"
 end
